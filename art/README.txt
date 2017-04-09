7c92402940944d7f1222d8a68857c554825e4065
Jared <jrddunbr@gmail.com>
2017-04-09 19:06:53 -0400

    Working (somewhat) - but we can start

---
+++ art/README.txt
@@ -0,0 +1,3 @@
+Thanks Graham Northup for the wonderful honeypot images! banner1-9 are of his creation
+
+The rest are of my creation
--- honeypot.py
+++ honeypot.py
@@ -1,18 +1,73 @@
-DEBUG_NET = False
+import socket
+import threading
+import datetime
 
-def tcp_server():
-    print ("starting server server on TCP port 2323")
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("0.0.0.0", 2323))
-    sock.listen(1)
+threads = []
+
+art_text = []
+
+HOST = "0.0.0.0"
+PORT = 3333
+ART_PATH = "./art"
+
+def generateFilename(client):
+    return "{}~{}.log".format(client.getpeername()[0].replace('.','_'), datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+
+def handler(client, address):
+    filename = "logs/" + generateFilename(client)
+    ip = client.getpeername()[0]
+    print("[Info]: New Client: {}".format(ip))
+    report = open(filename, "w+");
+    welcometext = "welcome\n"
+    client.send(welcometext.encode("utf-8"))
+    report.write(welcometext);
     while True:
         try:
-            conn, addr = sock.accept()
-            data = conn.recv(1024)
+            data = client.recv(1024).decode("utf-8")
+            report.write(data);
             if data:
-                message = data.decode("utf-8")
-                if DEBUG_NET: print ("recieved message: " + message + " from " + str(addr[0]))
-        except Exception as e:
-            print (e)
-            pass
+                if "exit\r\n" in str(data):
+                    #print("Client executed \"exit\" and disconnected")
+                    client.close()
+                    report.close()
+                    break
+                if "quit\r\n" in str(data):
+                    #print("Client executed \"quit\" and disconnected")
+                    client.close()
+                    report.close()
+                    break
+                #print(data)
+            else:
+                #print("Client disconnected")
+                client.close()
+                report.close()
+                break
+        except:
+            #print("Client disconnected")
+            client.close()
+            report.close()
+            break
+    print("[Info]: Lost Client: {}".format(ip))
+
+# Start TCP Stream, with automatic resource deallocation (so that the resource can be reused)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# Bind server to HOST and PORT
+server.bind((HOST, PORT))
+# Listen to up to 100 connections
+server.listen(100)
+while True:
+    # While we have clients..
+    client, address = server.accept()
+    # ... make sure they don't go over 60 seconds of inactivity ...
+    client.settimeout(60)
+    # ... and start it in a new thread.
+    thread = threading.Thread(target=handler, args = (client, address))
+    thread.setDaemon(True)
+    thread.start()
+    threads.append(thread)
+    count = 0
+    for thread in threads:
+        if thread.isAlive():
+            count += 1
+    print("[Info]: Number of clients: " + str(count))
